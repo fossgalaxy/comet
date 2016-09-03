f7161bc3d4bfc1693ae8216f47a725b02f5d97e1
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-09-03 18:02:01 +0100

    setup context data for future use

--- fg_users/views.py
+++ fg_users/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class ProfileView(TemplateView):
     template_name = "fg_users/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        return context
