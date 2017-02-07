1677e8445601c3919b2b86ffc63ec4b8c726437d
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-02-07 15:38:13 +0000

    change serialiser to work correctly

--- fg_competitions/serializers.py
+++ fg_competitions/serializers.py
@@ -7,18 +7,24 @@ class CompetitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Competition
 
-class UploadSerializer(serializers.ModelSerializer):
+class UploadReadOnlySerializer(serializers.ModelSerializer):
     upload = serializers.FileField(use_url=False)
     class Meta:
         model = SubmissionUpload
         fields = ('pk', 'upload', 'status', 'created', 'feedback')
 
+class UploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubmissionUpload
+        fields = ('pk', 'status', 'created', 'feedback')
+
 class SubmissionSerializer(serializers.ModelSerializer):
-    current_upload = UploadSerializer(read_only=True)
+    current_upload = UploadReadOnlySerializer(read_only=True)
     owner = serializers.SlugRelatedField(slug_field='username', read_only=True)
     class Meta:
         model = Submission
         fields = ('pk', 'name', 'owner', 'ranking', 'ranking_rd', 'velocity', 'current_upload')
+	read_only_fields = ["pk", "owner"]
 
 class TrackSerializer(serializers.ModelSerializer):
     submission_set = SubmissionSerializer(many=True, read_only=True)
