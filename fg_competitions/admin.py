f74a942e6b2759d675c3461896aab56a8731d764
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-05-16 12:17:07 +0100

    cleanup the admin area for submissions

--- fg_competitions/admin.py
+++ fg_competitions/admin.py
@@ -9,14 +9,29 @@ class CompetitionAdmin(admin.ModelAdmin):
     inlines = [
         CompetitionLinkInline
     ]
-
 admin.site.register(Competition, CompetitionAdmin)
-admin.site.register(Track)
+
+class TrackAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("competition", "allow_submit", "allow_update")
+    list_display = ("__str__", "allow_submit", "allow_update")
+admin.site.register(Track, TrackAdmin)
 
 class SubmissionUploadInline(admin.TabularInline):
     model = SubmissionUpload
+    extra = 1
 
 class SubmissionAdmin(admin.ModelAdmin):
+    fieldsets = (
+      (None, {
+          'fields': ('name', 'track', ('owner', "sample"), 'description')
+      }),
+      ("Ranking", {
+          'fields': ('ranking', 'ranking_rd', 'velocity')
+      })
+    )
+    list_filter = ("track",)
+    list_display = ("__str__", "track")
     inlines = [
         SubmissionUploadInline
     ]
