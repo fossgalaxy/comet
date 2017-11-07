5be7ad754c1dbbbfd20ef14bde94d3bc60eb697d
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-11-07 17:03:35 +0000

    drop down login form

--- fg_users/urls.py
+++ fg_users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^profile/', views.ProfileView.as_view()),
+    url(r'^profile/', views.ProfileView.as_view(), name="account_profile"),
     url(r'^', include('allauth.urls'))
 ]
