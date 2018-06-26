a0e43e9ea0d78e7af8512e33ffd8ae4cfa9d67c1
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2018-06-26 23:44:42 +0100

    allow docker configuration for email settings

--- comet/settings/deploy.py
+++ comet/settings/deploy.py
@@ -19,6 +19,20 @@ SSL_ONLY = bool(os.environ.get('SSL_ONLY', False))
 CSRF_COOKIE_SECURE = SSL_ONLY
 SESSION_COOKIE_SECURE = SSL_ONLY
 
+# Email stuff
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'noreply@fossgalaxy.com')
+DEFAULT_FROM_EMAIL = SERVER_EMAIL # not sure why there are two settings >.<
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = True # No, i'm not letting you turn this off.
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
+# People to email with django explodes
+ADMINS = os.environ.get('ADMINS', '').split(",")
+
 FILEPROVIDER_NAME = "nginx"
 
 # Database Related
