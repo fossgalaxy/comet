27474165b96ef0d49cbb58bac2413d5a5d300bce
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2018-11-21 16:34:12 +0000

    use buildah if installed to allow rootless builds

--- docker.sh
+++ docker.sh
@@ -2,5 +2,10 @@
 # build and push the docker image
 set -e
 
-docker build -t docker.io/fossgalaxy/comet .
-docker push docker.io/fossgalaxy/comet
+if [ -f /bin/buildah ]; then
+	buildah bud --label fossgalaxy/comet -t snapshot .
+	#buildah push fossgalaxy/comet:snapshot docker://docker.io/fossgalaxy/comet:snapshot
+else
+	docker build -t docker.io/fossgalaxy/comet .
+	docker push docker.io/fossgalaxy/comet
+fi
