03c41f2f79439678a770f9b85b017e012070761f
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2019-10-24 12:31:29 +0100

    avoid duplicate calls to competition table in filter view

--- fg_competitions/filters.py
+++ fg_competitions/filters.py
@@ -5,6 +5,11 @@ from .models import Track
 class TrackFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
 
+    def filter_queryset(self, queryset):
+        qs = super(TrackFilter, self).filter_queryset(queryset)
+        return qs.prefetch_related('competition')
+
     class Meta:
         model = Track
         fields = ["name", "allow_submit", "competition"]
+
